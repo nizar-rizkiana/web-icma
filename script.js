ac4e5519cf4e9c08eb9a3ef0ea3c33ac16d1c60e
nizar-rizkiana <nizar.rizkiana007@gmail.com>
2024-01-07 15:38:03 +0700

    menambahkan halaman ppdb

--- script.js
+++ script.js
@@ -24,16 +24,16 @@ window.onscroll = function(){
 
 
 
-const cursor = document.querySelector('#cursor'),
-        html = document.querySelector('html');
-html.addEventListener('mousemove', function(event){
-    setTimeout(function(){
-    let x = event.clientX;
-    let y = event.clientY;
-    cursor.style.left = `${x}px`;
-    cursor.style.top = `${y}px`;
-    }, 100);
-});
+// const cursor = document.querySelector('#cursor'),
+//         html = document.querySelector('html');
+// html.addEventListener('mousemove', function(event){
+//     setTimeout(function(){
+//     let x = event.clientX;
+//     let y = event.clientY;
+//     cursor.style.left = `${x}px`;
+//     cursor.style.top = `${y}px`;
+//     }, 100);
+// });
 
 
 const burger = document.querySelector('.burger');
